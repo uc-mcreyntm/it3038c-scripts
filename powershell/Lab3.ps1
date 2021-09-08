3d120f2491123a38b3e3b9dbe4ef0e61d2f34170
Tommy <mcreyntm@ucmail.uc.edu>
2021-09-08 00:13:35 -0400

    Lab 3 script

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,41 @@
+﻿function getIP {
+
+(Get-NetIPAddress).IPv4Address | Select-String "192*"
+
+
+}
+function getHostname {
+
+hostname
+
+}
+function getVersion {
+
+$HOST.Version.Major
+
+}
+
+function getDate {
+
+get-date -Format D
+
+}
+
+$HOSTVERSION = getVersion
+$DATE = getDate
+
+$IP = getIP
+$hostname = getHostname
+
+
+function createBody {
+
+"This machine's IP is $IP. User is $env:USERNAME. The Hostname is $hostname. Powershell version $HOSTVERSION. Today's Date is $DATE"
+
+}
+
+$Body = createBody
+
+Write-Output($Body)
+
+$Body | Out-File C:\it3038c-scripts\powershell\machineinfo.txt
