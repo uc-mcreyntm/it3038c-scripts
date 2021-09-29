9b3aeb65b337a8c3cb35cffc5c529025c76af761
root <root@UbuntuCT.heyk20.local>
2021-09-29 02:21:17 -0400

    Add project1 script

---
+++ project1/RouterBackup.sh
@@ -0,0 +1,7 @@
+if cmp -s `ls /var/www/private/RouterConfigs -t | head -n1` <(ssh router 'cat /config/config.boot'); then
+	
+	:
+
+else
+	/usr/bin/scp router:/config/config.boot /var/www/private/RouterConfigs/config.boot.`date +\%A\%B\%d`
+fi
