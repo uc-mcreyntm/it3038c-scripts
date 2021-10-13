bcb5fb53d7d42cac1dc100cd3449e9a93d467f53
root <root@UbuntuCT.heyk20.local>
2021-10-12 23:11:17 -0400

    Create Lab7 Script

---
+++ powershell/Lab7.ps1
@@ -0,0 +1,24 @@
+﻿function FindSheets {
+
+$path = Read-Host -Prompt 'Enter path'
+
+dir $path -r *.xlsx | Get-ExcelFileSummary | ft
+}
+
+function ReadSheet {
+
+$excelpath = Read-Host -Prompt 'Enter path'
+
+Import-Excel -Path $excelpath
+
+
+}
+
+function SheetInfo {
+
+$sheetpath = Read-Host -Prompt 'Enter Path'
+
+Get-ExcelWorkbookInfo -Path $sheetpath
+
+}
+
