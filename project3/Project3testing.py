e920e355180b64f6c01434cfb732785b56ed0fcc
uc-mcreyntm <mcreyntm@mail.uc.edu>
2021-11-14 02:27:13 -0500

    Retrying with subprocess instead of OS

--- project3/Project3testing.py
+++ project3/Project3testing.py
@@ -1,12 +1,24 @@
+import subprocess
 import os
+import sys
+
+DEVNULL = open(os.devnull)
 #Here, I will list all of the servers that I want to update
 servers = ['proxmox', 'vpn', 'ubuntu', 'plex']
 
-
+#Update all of the servers listed in the array "servers"
 for x in servers:
-    testing = os.popen('ssh {} hostname'.format(x))
-    output = testing.read()    
-    print(output)
+ main = subprocess.Popen('ssh {} apt update'.format(x), shell=False, stdout=DEVNULL)
+
+# After apt update is ran, it will allow the servers to see which packages need to be updated.
+
+#Run apt list --upgradeable to find any applications that need to be upgraded
+
+#for x in servers:
+    #upgrade = subprocess.Popen("ssh {} apt list --upgradeable".format(x))
+
+  #  output = upgrade.read()
+  #  print(output)
 
 
 
