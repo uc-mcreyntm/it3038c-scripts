9a347b2c239d2db084430f2362992251a9614523
uc-mcreyntm <mcreyntm@mail.uc.edu>
2021-11-16 00:27:34 -0500

    Created variables for output file path

--- project3/Project3.py
+++ project3/Project3.py
@@ -6,6 +6,9 @@ from sys import stdout
 servers = ['proxmox', 'vpn', 'ubuntu', 'plex']
 upgradeList = []
 
+unformatted = "Z:/html/upgrades/indexunformat.html"
+formatted = "Z:/html/upgrades/index.html"
+
 
 for x in servers:
     update = os.popen('ssh {} apt-get update'.format(x))
@@ -25,17 +28,17 @@ for x in servers:
 
 
    #Create/Overwrite file so that it is empty
-with open("Z:/html/upgrades/indexunformat.html", "w") as newfile:
+with open(unformatted, "w") as newfile:
     newfile.write("")
 
 # Append data to file
-with open("Z:/html/upgrades/indexunformat.html", "a") as writefile:
+with open(unformatted, "a") as writefile:
     writefile.writelines(upgradeList)
 
 #Put a new line after each application that needs upgraded for easier reading
 
-with open("Z:/html/upgrades/indexunformat.html", "r") as input:
-    with open("Z:/html/upgrades/index.html", "w") as output:
+with open(unformatted, "r") as input:
+    with open(formatted, "w") as output:
         for lines in input:
             lines = lines.rstrip('\n') + '<br>'
             print(lines, file=output)
